4c94584c76cf11f656aab891326b34200d2b34ee
Kevin Yokley <kyokley2@gmail.com>
2018-07-25 11:49:32 -0500

    Adding secret store app

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     ),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
+    path('secret/', include('passshare.secret_store.urls')),
 ] + static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
 )
