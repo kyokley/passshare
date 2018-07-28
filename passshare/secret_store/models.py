b842661de913de1f11d07dfafc8354cb709639ac
Kevin Yokley <kyokley2@gmail.com>
2018-07-28 18:11:36 -0500

    Start inherited Secret models

--- passshare/secret_store/models.py
+++ passshare/secret_store/models.py
@@ -12,5 +12,22 @@ class Secret(models.Model):
     data = models.TextField(null=False,
                             blank=False,
                             )
+    viewers = models.ManyToManyField(settings.AUTH_USER_MODEL)
     date_created = models.DateField(auto_now_add=True)
     date_edited = models.DateField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class TextSecret(Secret):
+    pass
+
+class FileSecret(Secret):
+    filename = models.CharField(max_length=32,
+                                null=False,
+                                blank=False)
+
+class UPSecret(Secret):
+    username = models.CharField(max_length=32,
+                                null=False,
+                                blank=False)
