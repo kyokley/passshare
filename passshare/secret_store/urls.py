ef47a03caa80965efb26e12f69b92619fa4e4071
Kevin Yokley <kyokley2@gmail.com>
2018-07-29 10:07:54 -0500

    Start work on templates

--- passshare/secret_store/urls.py
+++ passshare/secret_store/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
         path('', views.index, name='index'),
+        path('my_shares', views.my_shares, name='my_shares'),
+        path('recover', views.index, name='recover'),
 ]
--- passshare/secret_store/views.py
+++ passshare/secret_store/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('hello world')
+
+def my_shares(request):
+    context = {}
+    return render(request, 'secret_store/my_shares.html', context)
