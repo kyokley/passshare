d7e17095a44a55ca47dfcc89a9f7831aa4a3057c
Kevin Yokley <kyokley2@gmail.com>
2018-07-28 19:31:59 -0500

    Add countdown to Secret model

--- passshare/secret_store/models.py
+++ passshare/secret_store/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.conf import settings
 
+COUNTDOWN_CHOICES = (
+        (1, 1),
+        (7, 7),
+        (14, 14),
+        (30, 30),
+        (60, 60),
+        (90, 90),
+        )
 class Secret(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL,
                               on_delete=models.CASCADE)
@@ -15,6 +23,11 @@ class Secret(models.Model):
     viewers = models.ManyToManyField(settings.AUTH_USER_MODEL,
                                      related_name='%(class)s_related',
                                      )
+    countdown = models.IntegerField(null=False,
+                                    blank=False,
+                                    default=14,
+                                    choices=COUNTDOWN_CHOICES,
+                                    )
     date_created = models.DateField(auto_now_add=True)
     date_edited = models.DateField(auto_now=True)
 
