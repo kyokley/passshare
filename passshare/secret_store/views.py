3591b0abdc69a4bd0dd0d3a1e327cec6fcf427fd
Kevin Yokley <kyokley2@gmail.com>
2018-08-06 21:38:26 -0500

    Start Create page

--- passshare/secret_store/views.py
+++ passshare/secret_store/views.py
@@ -4,7 +4,22 @@ from django.http import HttpResponse
 from passshare.secret_store.models import TextSecret, UPSecret, FileSecret
 
 def create(request):
-    pass
+    user = request.user
+
+    # TODO: Make this raise a 403
+    if not user.is_authenticated:
+        raise Exception('User not authenticated')
+
+    text_shares = TextSecret.objects.filter(owner=user)
+    up_shares = UPSecret.objects.filter(owner=user)
+    file_shares = FileSecret.objects.filter(owner=user)
+
+    context = {'user': request.user,
+               'text_shares': text_shares,
+               'up_shares': up_shares,
+               'file_shares': file_shares,
+            }
+    return render(request, 'secret_store/create.html', context)
 
 def manage(request):
     user = request.user
--- passshare/static/js/project.js
+++ passshare/static/js/project.js
@@ -1,1 +1,23 @@
 /* Project specific Javascript goes here. */
+
+
+var N = 1024, r = 8, p = 1;
+
+var dkLen = 32;
+
+function gen_key(password, salt) {
+    password_buffer = new buffer.SlowBuffer(password.normalize("NFKC"));
+    salt_buffer = new buffer.SlowBuffer(salt.normalize("NFKC"));
+    scrypt(password_buffer, salt_buffer, N, r, p, dkLen, function(error, progress, key) {
+            if (error) {
+              console.log("Error: " + error);
+
+            } else if (key) {
+              console.log("Found: " + key);
+
+            } else {
+              // update UI with progress complete
+              console.log(progress);
+            }
+          });
+}
