007fac4a4b0a1d7f3a3be72fcf1cd51c62dd86f9
Kevin Yokley <kyokley2@gmail.com>
2018-09-16 20:22:35 -0500

    Register secret models on admin page

--- passshare/secret_store/admin.py
+++ passshare/secret_store/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from passshare.secret_store.models import (TextSecret,
+                                           FileSecret,
+                                           UPSecret,
+                                           )
 
 # Register your models here.
+admin.site.register(TextSecret)
+admin.site.register(FileSecret)
+admin.site.register(UPSecret)
